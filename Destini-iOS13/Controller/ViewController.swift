ea98e0041b45930f3463c19d237684fc2cf455b4
Sunday Ndu <ndusunday@gmail.com>
2021-09-07 18:07:39 +0100

    Made the simple project to follow MVC pattern.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUi()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.getSelection(choiceSelected: sender.currentTitle!)
+        updateUi()
+    }
+    
+    func updateUi() {
+        storyLabel.text = storyBrain.getCurrentStory().title
+        
+        choice1Button.setTitle(storyBrain.getCurrentStory().choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.getCurrentStory().choice2, for: .normal)
+    }
+    
 }
 
